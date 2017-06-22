b27be97d73634d36cdbdc7912f33b35428b2be4e
Omri A <devomri@gmail.com>
2017-06-23 00:10:41 +0300

    Added NN

---
+++ Model/DeepLearningKeras.py
@@ -0,0 +1,80 @@
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.preprocessing.text import Tokenizer
+import DataAccess.DataAccess as data_access
+import string
+
+
+dal = data_access.DataAccess()
+
+def convert_data(data):
+    x = list()
+    y = list()
+
+    for z in data:
+        x.append(convert_word_to_vec(z.x))
+        y.append(convert_label_to_vec(z.y))
+
+    return {
+        "x": np.array(x),
+        "y": np.array(y)
+    }
+
+
+def convert_word_to_vec(word):
+    vec = np.zeros(26)
+    for c in word.lower():
+        i = string.ascii_lowercase.index(c)
+        vec[i] += 1
+
+    return vec
+
+
+def convert_label_to_vec(label):
+    vec = np.zeros(5000)
+    i = dal.dictionary.index(label)
+    vec[i] = 1
+
+    return vec
+
+batch_size = 32
+epochs = 5
+
+print('Loading data...')
+train_raw = convert_data(dal.train_data)
+test_raw = convert_data(dal.test_data)
+
+x_train = train_raw["x"]
+y_train = train_raw["y"]
+x_test = test_raw["x"]
+y_test = test_raw["y"]
+
+num_classes = 5000
+
+#tokenizer = Tokenizer(num_words=max_words)
+#x_train = tokenizer.sequences_to_matrix(x_train, mode='binary')
+#x_test = tokenizer.sequences_to_matrix(x_test, mode='binary')
+
+print('Building model...')
+model = Sequential()
+model.add(Dense(512, input_shape=(26,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+score = model.evaluate(x_test, y_test,
+                       batch_size=batch_size, verbose=1)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
--- Model/DeepLearningMNIST.py
+++ Model/DeepLearningMNIST.py
@@ -1,11 +1,57 @@
 import numpy as np
 import tensorflow as tf
+import string
+import DataAccess.DataAccess as data_access
+
 
 class DeepLearningMNIST:
+    def __init__(self):
+        self.last_batch_index = 0
+        self.dal = data_access.DataAccess()
+        self.train = self.convert_data(self.dal.train_data)
+        self.test = self.convert_data(self.dal.test_data)
+
+    def convert_data(self, data):
+        x = list()
+        y = list()
+
+        for z in data:
+            x.append(self.convert_word_to_vec(z.x))
+            y.append(self.convert_label_to_vec(z.y))
+
+        return {
+            "x": np.array(x),
+            "y": np.array(y)
+        }
+
+    def convert_word_to_vec(self, word):
+        vec = np.zeros(26)
+        for c in word.lower():
+            i = string.ascii_lowercase.index(c)
+            vec[i] += 1
+
+        return vec
+
+    def convert_label_to_vec(self, label):
+        vec = np.zeros(5000)
+        i = self.dal.dictionary.index(label)
+        vec[i] = 1
+
+        return vec
+
+    def get_next_batch(self, batch_size):
+        start_index = self.last_batch_index
+        end_index = start_index + batch_size
+        batch_x = self.train["x"][start_index:end_index]
+        batch_y = self.train["y"][start_index:end_index]
+
+        self.last_batch_index += batch_size
+
+        return batch_x, batch_y
 
     def train_model(self):
         x = tf.placeholder(tf.float32, [None, 26])
-        W = tf.Variable(tf.zeros([30, 5000]))
+        W = tf.Variable(tf.zeros([26, 5000]))
         b = tf.Variable(tf.zeros([5000]))
         y = tf.matmul(x, W) + b
 
@@ -18,12 +64,12 @@ class DeepLearningMNIST:
         sess = tf.InteractiveSession()
         tf.global_variables_initializer().run()
         # Train
-        for _ in range(1000):
-            batch_xs, batch_ys = mnist.train.next_batch(100)
+        for _ in range(40):
+            batch_xs, batch_ys = self.get_next_batch(1000)
             sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
         # Test trained model
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                            y_: mnist.test.labels}))
+        print(sess.run(accuracy, feed_dict={x: self.test["x"],
+                                            y_: self.test["y"]}))
