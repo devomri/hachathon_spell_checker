d04dfaffe4c380342f6da7c402b56024a644a4d2
Omri A <devomri@gmail.com>
2017-06-22 17:47:58 +0300

    added data access

---
+++ DataAccess/DataAccess.py
@@ -0,0 +1,15 @@
+class DataAccess:
+
+    def __init__(self):
+        self.train_data = self.get_train_data()
+        self.test_data = self.get_test_data()
+        self.validation_date = self.get_validation_data()
+
+    def get_train_data(self):
+        return None
+
+    def get_train_data(self):
+        return None
+
+    def get_train_data(self):
+        return None
